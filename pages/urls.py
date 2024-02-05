11f07698fcb8081139a4a1abf4db7889ee29eff2
mike-gnzz <magzalez45@gmail.com>
2024-02-05 15:43:00 -0800

    added url paths to urls.py in pages app

--- pages/urls.py
+++ pages/urls.py
@@ -3,6 +3,8 @@ from pages import views
 from projects import views as pv
 urlpatterns = [
     path("", views.home, name='home'),
-    path('resume/', views.other_page, name='resume'),
-    path("projects/", pv.project_index, name='projects'),
+    path('about/', views.about_me, name='about'),
+    path("projects/", views.projects_page, name='projects'),
+    path("contact/", views.contact, name="contact"),
+
 ]
