30812d991698d415503ada2addad144cf97264f9
mike-gnzz <magzalez45@gmail.com>
2024-02-08 14:16:12 -0800

    had it include projects.urls instead of importing views

--- pages/urls.py
+++ pages/urls.py
@@ -1,10 +1,9 @@
-from django.urls import path
+from django.urls import path, include
 from pages import views
-from projects import views as pv
 urlpatterns = [
     path('home/', views.home, name='home'),
     path('about/', views.about, name='about'),
-    path('projects/', pv.project_index, name='projects'),
+    path('projects/', include('projects.urls')),
     path('contact/', views.contact, name='contact'),
 
 ]
