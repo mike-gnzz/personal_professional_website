2568e84dc2a6997180578ff216bc2b6adcc7675b
mike-gnzz <magzalez45@gmail.com>
2024-02-08 12:44:15 -0800

    added a variable for linked in and resume locations

--- pages/views.py
+++ pages/views.py
@@ -7,6 +7,7 @@ def home(request):
     email = "gnzz.michael@gmail.com"
     location = "California"
     LinkedIn_url = 'https://www.linkedin.com/in/michael-gonzalez-69935a178/'
+    resume_url = '/static/pages/PDF/Michael_Gonzalez_CV.pdf'
     projects = Project.objects.all()
     return render(request, "pages/home.html",{'projects':projects,
                                               'email':email,
