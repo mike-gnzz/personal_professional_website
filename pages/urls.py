aedc506cd01b03ae36baecc09eb0ab07cdaeba30
mike-gnzz <magzalez45@gmail.com>
2024-02-11 01:46:22 -0800

    updated the home url to include www.

--- pages/urls.py
+++ pages/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 from pages import views
 
 urlpatterns = [
-    path('michaelgnzz.me', views.home, name='home'),
+    path('www.michaelgnzz.me', views.home, name='home'),
     path('projects/', include('projects.urls')),
 
 ]
