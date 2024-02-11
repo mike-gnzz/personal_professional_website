23e6931e8ab1fa12a4dc320365327452b6441092
mike-gnzz <magzalez45@gmail.com>
2024-02-11 01:25:16 -0800

    changed the home url path to michaelgnzz.me

--- pages/urls.py
+++ pages/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 from pages import views
 
 urlpatterns = [
-    path('home/', views.home, name='home'),
+    path('michaelgnzz.me', views.home, name='home'),
     path('projects/', include('projects.urls')),
 
 ]
