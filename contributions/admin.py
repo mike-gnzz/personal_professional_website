547d48e46b34b32a9e34dcfd33d91ebb6bb62323
mike-gnzz <magzalez45@gmail.com>
2024-02-19 11:56:57 -0800

    adding the contributions model to the admin page

--- contributions/admin.py
+++ contributions/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from contributions.models import Contributions
 
 # Register your models here.
+
+class ContributionsAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(Contributions,ContributionsAdmin)
