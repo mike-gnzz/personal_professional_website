10402903187b18ef7bb6364f873e62de4376539c
mike-gnzz <magzalez45@gmail.com>
2024-02-19 11:28:37 -0800

    reverted the two files to exclude contributions as it made more sense to make them a seperate app

--- projects/models.py
+++ projects/models.py
@@ -6,14 +6,3 @@ class Project(models.Model):
     description = models.TextField()
     technology = models.CharField(max_length=50)
     image = models.FileField(upload_to="project_images/", blank=True)
-
-class Contributions(models.Model):
-    title = models.CharField(max_length=100)
-    description = models.TextField()
-    link = models.URLField(blank=True)
-    main_image = models.FileField(upload_to="contribution_images/", blank=True)
-    detail_images = models.ManyToManyField("DetailImage", blank=True)
-
-class Detailimage(models.Model):
-    image = models.FileField(upload_to="detail_files/")
-
