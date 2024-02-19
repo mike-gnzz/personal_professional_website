c134abfc56ce3e082c6755f748267b9ee5e9aa78
mike-gnzz <magzalez45@gmail.com>
2024-02-18 19:48:25 -0800

    updated admin page to include contributions

--- projects/admin.py
+++ projects/admin.py
@@ -3,8 +3,10 @@ from django.contrib import admin
 # Register your models here.
 from django.contrib import admin
 from projects.models import Project
+from projects.models import Contributions
 
 class ProjectAdmin(admin.ModelAdmin):
     pass
 
 admin.site.register(Project, ProjectAdmin)
+admin.site.register(Contributions)
