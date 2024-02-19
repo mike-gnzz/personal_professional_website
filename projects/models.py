61e221ccd61bf2bed8e656295204021e5174fced
mike-gnzz <magzalez45@gmail.com>
2024-02-18 19:47:56 -0800

    created a new model called contributions to list non coding project related contributions

--- projects/models.py
+++ projects/models.py
@@ -6,3 +6,10 @@ class Project(models.Model):
     description = models.TextField()
     technology = models.CharField(max_length=50)
     image = models.FileField(upload_to="project_images/", blank=True)
+
+class Contributions(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    link = models.URLField(blank=True)
+    image = models.FileField(upload_to="contribution_images/", blank=True)
+    
