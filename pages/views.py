895ceac39bb84fbe45147f57f19daca12e63c3a4
mike-gnzz <magzalez45@gmail.com>
2024-02-21 17:51:11 -0800

    renamed the resume url again to be static url instead

--- pages/views.py
+++ pages/views.py
@@ -1,14 +1,14 @@
 from django.shortcuts import render
 from projects.models import Project
 from contributions.models import Contributions
-from personal_portfolio.settings import BASE_DIR
+from personal_portfolio.settings import STATIC_URL
 # Create your views here.
 
 def home(request):
     email = "gnzz.michael@gmail.com"
     location = "California"
     LinkedIn_url = 'https://www.linkedin.com/in/michael-gonzalez-69935a178/'
-    resume_url = str(BASE_DIR / 'pages/static/PDF/Michael_Gonzalez_CV.pdf')
+    resume_url = STATIC_URL / 'pages/PDF/Michael_Gonzalez_CV.pdf'
     projects = Project.objects.all()
     contributions = Contributions.objects.all()
     return render(request, "pages/home.html",{'projects':projects,
