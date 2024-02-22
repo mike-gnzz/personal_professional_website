1b703cba6e2964b7da25ae34f96a3e7079b2e031
mike-gnzz <magzalez45@gmail.com>
2024-02-21 18:04:30 -0800

    fixed issue from local machine test with resume link

--- pages/views.py
+++ pages/views.py
@@ -1,14 +1,14 @@
 from django.shortcuts import render
 from projects.models import Project
 from contributions.models import Contributions
-from personal_portfolio.settings import STATIC_URL
+from personal_portfolio.settings import BASE_DIR
 # Create your views here.
 
 def home(request):
-    email = "gnzz.michael@gmail.com"
+    email = "gnzz.michael@gmail.com"    
     location = "California"
     LinkedIn_url = 'https://www.linkedin.com/in/michael-gonzalez-69935a178/'
-    resume_url = STATIC_URL / 'pages/PDF/Michael_Gonzalez_CV.pdf'
+    resume_url = 'pages/PDF/Michael_Gonzalez_CV.pdf'
     projects = Project.objects.all()
     contributions = Contributions.objects.all()
     return render(request, "pages/home.html",{'projects':projects,
