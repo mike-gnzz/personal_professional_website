8d3836b848c08389468eb50470246fe2d9498a58
mike-gnzz <magzalez45@gmail.com>
2024-02-21 17:42:30 -0800

    changed the route for the resume_url

--- pages/views.py
+++ pages/views.py
@@ -1,14 +1,14 @@
 from django.shortcuts import render
 from projects.models import Project
 from contributions.models import Contributions
-
+from personal_portfolio.settings import BASE_DIR
 # Create your views here.
 
 def home(request):
     email = "gnzz.michael@gmail.com"
     location = "California"
     LinkedIn_url = 'https://www.linkedin.com/in/michael-gonzalez-69935a178/'
-    resume_url = '/pages/static/PDF/Michael_Gonzalez_CV.pdf'
+    resume_url = str(BASE_DIR / 'pages/static/PDF/Michael_Gonzalez_CV.pdf')
     projects = Project.objects.all()
     contributions = Contributions.objects.all()
     return render(request, "pages/home.html",{'projects':projects,
